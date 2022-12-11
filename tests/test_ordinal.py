69a756e751e6514ae88b4324620a593d253da2d7
Vasyl Spachynskyi <vspachyn@gmail.com>
2022-12-11 13:51:10 +0200

    Extend ordinals with all possibles forms

--- tests/test_ordinal.py
+++ tests/test_ordinal.py
@@ -4,16 +4,24 @@ from ukr.wfst import apply_fst_text, graph
 
 
 @pytest.mark.parametrize('spoken,expected', [
-    ('пeрший', '1'),
+    ('перший', '1'),
     ('дванадцятий', '12'),
     ('двадцятий', '20'),
     ('девяносто девяте', '99'),
     ('тридцять другий', '32'),
     ('девятсот дванадцятий', '912'),
     ('дві тисячі девятисотий', '2900'),
-    ('дві тисячі пeрший', '2001'),
+    ('дві тисячі перший', '2001'),
     ('дві тисячі двадцять другого', '2022'),
     ('дві тисячі девятнадцятого', '2019'),
 ])
 def test_ordinal(spoken, expected):
+    assert apply_fst_text(spoken, graph) == expected
+
+
+@pytest.mark.parametrize('spoken,expected', [
+    ('я пішов двадцять першого числа до монастиря', 'я пішов 21 числа до монастиря'),
+    ('ти народилася двадцять першого числа чи двадцять восьмого', 'ти народилася 21 числа чи 28'),
+])
+def test_ordinal_with_other_words(spoken, expected):
     assert apply_fst_text(spoken, graph) == expected
