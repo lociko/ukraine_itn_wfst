bbe3d5fc39c01a8f8119423887b5ade30ea383fd
Vasyl Spachynskyi <vspachyn@gmail.com>
2023-02-04 23:05:16 +0200

    WIP: Include JSON option for command line util

--- ukr/__main__.py
+++ ukr/__main__.py
@@ -1,14 +1,17 @@
 import argparse
 import sys
 
-from ukr.wfst import graph, apply_fst_text
+from ukr.wfst import graph, json_graph, apply_fst_text
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--inverse', action='store_true', help='inverse inverse text normalization')
+parser.add_argument('-j', '--json', action='store_true', help='inverse inverse text normalization')
 args = parser.parse_args()
 
 if args.inverse:
     graph = graph.invert()
 
+main_graph = json_graph if args.json else graph
+
 for line in sys.stdin:
-    print(apply_fst_text(line.strip(), graph))
+    print(apply_fst_text(line.strip(), main_graph))
